d29b9c137e8922f0e11aba2cebd14edfa1efa287
lv0817 <412435484@qq.com>
2018-03-13 17:54:32 +0800

    subplot

---
+++ subplot.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt 
+
+plt.figure()
+
+plt.subplot(2,1,1)
+plt.plot([0,1],[0,2])
+
+plt.subplot(2,3,4)
+plt.plot([0,1],[0,2])
+
+plt.subplot(2,3,5)
+plt.plot([0,1],[0,2])
+
+plt.subplot(2,3,6)
+plt.plot([0,1],[0,2])
+
+plt.show()
